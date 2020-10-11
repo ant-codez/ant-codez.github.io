d973191df4aa56212fc1929ec3c9063f94c295f2
ant-codez <ajserebii@gmail.com>
2020-10-10 18:38:06 -0700

    Updates

---
+++ precache-manifest.5f7ab9562f6fedc45c60e03b01c92509.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54beedb0017b7843a1a2fbf5ed96c2af",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "3ddc43b50ce5f09bcec8",
+    "url": "/Portfolio/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "3d0fb2eb97a8d97acbdf",
+    "url": "/Portfolio/static/css/main.71f667f8.chunk.css"
+  },
+  {
+    "revision": "3ddc43b50ce5f09bcec8",
+    "url": "/Portfolio/static/js/2.791b4a66.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/Portfolio/static/js/2.791b4a66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d0fb2eb97a8d97acbdf",
+    "url": "/Portfolio/static/js/main.494a4705.chunk.js"
+  },
+  {
+    "revision": "a3cd34b26739dcad7bbe",
+    "url": "/Portfolio/static/js/runtime-main.b2973787.js"
+  }
+]);
